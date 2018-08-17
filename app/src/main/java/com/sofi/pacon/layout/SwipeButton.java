ad467881bce847847c96912c47e2e0c2602fe7c1
ssoffy <sufy@wanadoo.fr>
2018-08-17 14:55:21 +0200

    Adding icon on main activity

---
+++ app/src/main/java/com/sofi/pacon/layout/SwipeButton.java
@@ -0,0 +1,66 @@
+package com.sofi.pacon.layout;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.sofi.pacon.R;
+
+public class SwipeButton extends RelativeLayout {
+
+    private String label;
+    private Drawable drawableIcon;
+
+    private TextView textView1;
+    private ImageView icon;
+    private RelativeLayout layout;
+
+    public SwipeButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public SwipeButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.swipe_button, this);
+        this.textView1 = findViewById(R.id.header);
+        this.icon = findViewById(R.id.icon);
+        this.layout = findViewById(R.id.layout_bg);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        init();
+        // Getting the attributes.
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.SwipeButton, 0, 0);
+        try {
+            label = a.getString(R.styleable.SwipeButton_label);
+            drawableIcon = a.getDrawable(R.styleable.SwipeButton_icon);
+        } finally {
+            a.recycle();
+        }
+        // Setting the attributes to view.
+        this.textView1.setText(label);
+        this.icon.setImageDrawable(drawableIcon);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        // TODO Auto-generated method stub
+        super.onWindowFocusChanged(hasFocus);
+        // Getting the height of layout.
+        int h = layout.getHeight();
+        // I can't set icon height here so I set the padding.
+        int p = h / 4;
+        this.icon.setPadding(p, p, p, p);
+    }
+}
